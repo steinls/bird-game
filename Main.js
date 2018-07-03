6b0cb8a6c5a69020f9c03d6c859605d59bf39c5f
steinls <3026235413@qq.com>
2018-07-03 15:24:54 +0800

    封装小游戏资源加载器

--- Main.js
+++ Main.js
@@ -1,6 +1,9 @@
+import {ResourceLoader} from './js/base/ResourceLoader.js'
+
 // 初始化游戏，游戏入口
 export class Main {
 	constructor() {
+		new ResourceLoader();
 		console.log('main 执行了')
 	}
 }
--- js/base/ResourceLoader.js
+++ js/base/ResourceLoader.js
@@ -1,8 +1,30 @@
-import {Resources} from './Resources.js'
+import {Resources} from './Resources.js';
 
 // 在图片加载完成后，启动游戏
 export class ResourceLoader {
 	constructor() {
-		this.map = new map(Resources)
+		this.map = new Map(Resources);
+		console.log(this.map);
+		for (let [key, value] of this.map) {
+			const image = new Image();
+			image.src = value;
+			this.map.set(key, image);
+		}
+	}
+
+	onLoaded(callback) {
+		let loadedCount = 0;
+		for (let value of this.map.value()) {
+			value.onload = () => {
+				loadedCount++;
+				if (loadedCount >= this.map.size) {
+					callback(this.map);
+				}
+			}
+		}
+	}
+
+	static create() {
+		return new ResourceLoader();
 	}
 }
