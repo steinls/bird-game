eb364987454c523353089040020919777571748d
steinls <3026235413@qq.com>
2018-07-03 18:16:18 +0800

    制作变量缓存器，并进行测试

--- Main.js
+++ Main.js
@@ -1,26 +1,23 @@
 import {ResourceLoader} from './js/base/ResourceLoader.js'
 import {Director} from './js/Director.js'
 import {BackGround} from './js/runtime/BackGround.js'
+import {DataStore} from './js/base/DataStore.js'
 
 // 初始化游戏，游戏入口
 export class Main {
 	constructor() {
 		this.canvas = document.querySelector('canvas');
 		this.ctx = this.canvas.getContext('2d');
+		this.dataStore = DataStore.getInstance();
+		console.log(this.dataStore)
 		const loader = ResourceLoader.create();
 		loader.onLoaded(map => this.onResourceLoaderFirstLoaded(map))
-
-
 	}
 	onResourceLoaderFirstLoaded(map) {
-		let image = map.get('background');
-		// this.ctx.drawImage(
-		// 	image,
-		// 	0,0,image.width,image.height,
-		// 	0,0,image.width,image.height
-		// );
-		// console.log(map.get('background').src)
-		let bg = new BackGround(this.ctx,image);
+		this.dataStore.ctx = this.ctx;
+		this.dataStore.map = map;
+		// console.log(map)
+		let bg = new BackGround(this.ctx,map.get('background'));
 		bg.draw();
 	}
 }
--- js/base/DataStore.js
+++ js/base/DataStore.js
@@ -1,4 +1,29 @@
 // 变量缓存器
 export class DataStore {
+	static getInstance() {
+		if(!DataStore.instance){
+			DataStore.instance = new DataStore();
+		}
+		return DataStore.instance;
+	}
 
+	constructor(){
+		this.map = new Map();
+	}
+
+	put(key,value){
+		this.map.set(key,value);
+		console.log(111)
+		return this;
+	}
+
+	get(key){
+		return this.map.get(key);
+	}
+
+	destory(){
+		for (let value of this.map.values()) {
+			value = null;
+		}
+	}
 }
