b963783deb7160da7a887416e79de84adc0c15eb
steinls <3026235413@qq.com>
2018-07-05 12:17:15 +0800

    让小鸟动起来

--- js/player/Birds.js
+++ js/player/Birds.js
@@ -9,6 +9,44 @@ export class Birds extends Sprite {
 			srcW:img.width,srcH:img.height,
 			w:img.width,h:img.height
 		});
+
+		// es2018对象的可以用...展开的，这里就先规划好吧
+		this.position = {
+			x:window.innerWidth/4,
+			y:window.innerHeight/2,
+			w:32,
+			h:24
+		};
+		this.clips = [
+			{
+				srcX:8,
+				srcY:11,
+				srcW:32,
+				srcH:24
+			},
+			{
+				srcX:60,
+				srcY:11,
+				srcW:32,
+				srcH:24
+			},
+			{
+				srcX:113,
+				srcY:11,
+				srcW:32,
+				srcH:24
+			}
+		];
+		this.count = 0;
+		this.birdsSpeed = 0.3;
+		this.status = 0;
 	}
 
+	draw() {
+		this.count = (this.count+this.birdsSpeed)%3;
+		this.status = Math.floor(this.count);
+		let config = Object.assign({img:this.img},this.clips[this.status],this.position);
+		// console.log(config)
+		super.draw(config);
+	}
 }
